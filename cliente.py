8a8b115780a5417031d5475faf8989980487cf85
matheus <matheus@mail.tecmint.com>
2021-06-03 15:26:10 -0300

    Alterações e criação de comandos

--- cliente.py
+++ cliente.py
@@ -1,35 +1,62 @@
 import socket
 import threading
+import sys
 
 # Choosing Nickname
-nickname = input("Choose your nickname: ")
+print("Bem vindo ao Humortadela!!")
+nickname = input("Como deseja ser chamado: ")
 
 # Connecting To Server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 55555))
+client.connect(('127.0.0.1', 55558))
+
+encerrar_conexao = False
 
 # Listening to Server and Sending Nickname
 def receive():
     while True:
+        global encerrar_conexao
+        if encerrar_conexao:
+            break
+        
         try:
             # Receive Message From Server
             # If 'NICK' Send Nickname
-            message = client.recv(1024).decode('ascii')
+            message = client.recv(1024).decode('UTF-8')
             if message == 'NICK':
-                client.send(nickname.encode('ascii'))
+                client.send(nickname.encode('UTF-8'))
+            elif message == 'QUIT':
+                print("Conexão encerrada")
+                client.close()
+                encerrar_conexao = True
+                exit(0)
             else:
                 print(message)
         except:
             # Close Connection When Error
-            print("An error occured!")
+            print("Aconteceu um problema!")
             client.close()
+            exit(0)
             break
 
 # Sending Messages To Server
 def write():
     while True:
+        if encerrar_conexao:
+            break
+        
         message = '{}: {}'.format(nickname, input(''))
-        client.send(message.encode('ascii'))
+        if message[len(nickname)+2:].startswith('/'):
+            if message[len(nickname)+2:].startswith('/help'):
+                client.send('HELP'.encode('UTF-8'))
+            elif message[len(nickname)+2:].startswith('/list'):
+                client.send('LIST'.encode('UTF-8'))
+            elif message[len(nickname)+2:].startswith('/quit'):
+                client.send(f'QUIT {message[len(nickname)+2+6]}'.encode('UTF-8'))
+            else:
+                print("Comando inválido, use /help para ver os comandos disponíveis")    
+        else:
+            client.send(message.encode('UTF-8'))
 
 # Starting Threads For Listening And Writing
 receive_thread = threading.Thread(target=receive)
--- servidor.py
+++ servidor.py
@@ -3,7 +3,7 @@ import threading
 
 # Connection Data
 host = '127.0.0.1'
-port = 55555
+port = 55558
 
 # Starting Server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,47 +14,60 @@ server.listen()
 clients = []
 nicknames = []
 
-# Sending Messages To All Connected Clients
 def broadcast(message):
     for client in clients:
         client.send(message)
         
-# Handling Messages From Clients
 def handle(client):
     while True:
         try:
-            # Broadcasting Messages
-            message = client.recv(1024)
-            broadcast(message)
+            msg = message = client.recv(1024)
+            if msg.decode('UTF-8').startswith('HELP'):
+                client.send('Comandos Disponíveis:\n*/help - Mostra os comandos disponíveis\n*/list - Lista os usuários online\n*/quit nomeDoUsuario - Para sair do chat'.encode('UTF-8'))
+            elif msg.decode('UTF-8').startswith('LIST'):
+                print("teste list")
+            elif msg.decode('UTF-8').startswith('QUIT'):
+                usuario = msg.decode('UTF-8')[5:]
+                sair(usuario)
+                client.send('QUIT'.encode('UTF-8'))
+            else:
+                broadcast(message)
         except:
-            # Removing And Closing Clients
             index = clients.index(client)
             clients.remove(client)
+            client.send('QUIT'.encode('UTF-8'))
             client.close()
             nickname = nicknames[index]
-            broadcast('{} left!'.format(nickname).encode('ascii'))
-            nicknames.remove(nickname)
+            broadcast('{} saiu da sala!'.format(nickname).encode('UTF-8'))
+            nicknames.remove(nickname)            
             break
         
-# Receiving / Listening Function
+def sair(usuario):
+    if usuario in clients:
+        index = clients.index(usuario)
+        clients.remove(usuario)
+        usuario.close()
+        nickname = nicknames[index]
+        broadcast('{} saiu da sala!'.format(nickname).encode('UTF-8'))
+        nicknames.remove(nickname)    
+        
 def receive():
     while True:
-        # Accept Connection
+        print("Servidor online!!")
         client, address = server.accept()
-        print("Connected with {}".format(str(address)))
+        print("Conexão estabelecida com {}".format(str(address)))
 
-        # Request And Store Nickname
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
+        client.send('NICK'.encode('UTF-8'))
+        nickname = client.recv(1024).decode('UTF-8')
         nicknames.append(nickname)
         clients.append(client)
+        
+        print("O Apelido é {}".format(nickname))
+        broadcast("{} entrou na sala! Bem vindo!\n".format(nickname).encode('UTF-8'))
+        client.send('Conexão estabelecida com o servidor!'.encode('UTF-8'))
 
-        # Print And Broadcast Nickname
-        print("Nickname is {}".format(nickname))
-        broadcast("{} joined!".format(nickname).encode('ascii'))
-        client.send('Connected to server!'.encode('ascii'))
-
-        # Start Handling Thread For Client
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
-receive()
+receive()        
+        
+
